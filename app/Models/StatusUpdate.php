a9244d4ed1901261de85da0e600d67234da78cff
bloodykheeng <bloodykheeng@gmail.com>
2024-03-26 11:43:00 +0300

    ädded model status

---
+++ app/Models/StatusUpdate.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Spatie\ModelStatus\Status;
+
+class StatusUpdate extends Status
+{
+    protected $fillable = ['user_id', 'email']; // Ensure user_id and email are mass assignable
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($statusUpdate) {
+            if (empty($statusUpdate->user_id) && auth()->check()) {
+                $statusUpdate->user_id = auth()->id() ?? null;
+                $statusUpdate->email = auth()->user()->email ?? null; // Set email from the authenticated user
+            }
+        });
+    }
+
+    // Relation to User
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
